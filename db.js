5028049b9db5462e00a5683192cdb5780d14f2d4
Maarten Dorrestijn <gdorrestijn@student.ru.nl>
2017-10-07 10:24:56 -0400

    test

--- db.js
+++ db.js
@@ -10,6 +10,9 @@ var dbUrl = 'mongodb://localhost/';
 if (process.env.OPENSHIFT_MONGODB_DB_HOST) {
 	dbUrl = 'mongodb://admin:kWfMTF99y7iy@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/'
 }
+
+dbUrl = 'mongodb://admin:kWfMTF99y7iy@mongodb/admin'
+
 MongoClient.connect(dbUrl, function(err, db) {
     if(err) throw err;
     exports.db = db;
